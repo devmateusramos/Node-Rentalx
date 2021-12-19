b6613c97b4fe1c5871921bb6f6bd8d7f995c530e
devmateusramos <devmateusramos@gmail.com>
2021-12-19 16:55:57 -0300

    criando controller de usuario

--- src/database/migrations/1639940310811-CreateUsers.ts
+++ src/database/migrations/1639940310811-CreateUsers.ts
@@ -24,6 +24,10 @@ export class CreateUsers1639940310811 implements MigrationInterface {
             type: "varchar",
           },
           {
+            name: "email",
+            type: "varchar",
+          },
+          {
             name: "driver_license",
             type: "varchar",
           },
